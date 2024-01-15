f710247b2b0592e09afec15b1c1565d6e6307a34
Swift <swiftshineexists@gmail.com>
2024-01-15 11:28:52 -0600

    update project settings

--- include/game/gamemgr.h
+++ include/game/gamemgr.h
@@ -9,7 +9,7 @@ public:
     GameMgr();
 public:
     void* baseObject;           // 0x0
-    u8 unk1[0x6C - 0x4];        // 0x4
+    u8 _4[0x6C - 0x4];          // 0x4
     GFL::Task* stageTask;       // 0x6C
     u8 unk2[0x84 - 0x70];       // 0x70
     GFL::Task* task2;           // 0x84
@@ -17,7 +17,7 @@ public:
     void* functorClassMethod;   // 0x90
     u8 unk4[0xA0 - 0x94];       // 0x94
     void* buffer;               // 0xA0
-    u8 unk5[0xBC - 0xA4];       // 0xA4
+    u8 unk5[0xC0 - 0xA4];       // 0xA4
 };
 
 static_assert(sizeof(GameMgr) == 0xC0, "GameMgr is not the correct size.");
