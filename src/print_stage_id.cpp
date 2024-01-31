dabba8ef1f4cfdd29a3825e8564082ec029be62b
Swift <swiftshineexists@gmail.com>
2024-01-31 06:34:32 -0600

    print stage id without killing the game

--- src/print_stage_id.cpp
+++ src/print_stage_id.cpp
@@ -2,30 +2,56 @@
 #include "gfl/gfl.h"
 #include "game/stage/stage.h"
 
-
 u32* printStageID(u32* stageTask, Stage* stage) {
     OSReport("Loading stage with ID: %d\n", stage->stageID);
     return stageTask;
 }
 
 // Hook into StageMgr::loadStageData
-// This accurately prints that i believe to be the stage ID,
-// though it causes the *new* level data to not be loaded,
-// meaning the current level loads in its place. hm.
-
 // uncomment below if you want to use this (currently broken but somewhat functional)
-/*kmBranchDefAsm(0x80044b34, 0x80044b38) {
+kmBranchDefAsm(0x80044b34, 0x80044b38) {
     // original instruction
     lwz r0, 0x0(r4);
 
-    // save r3 and r4
-    stw r3, -0x4(r1);
-    stw r4, -0x8(r1);
+    // save volatile registers
+    stwu sp, -0x3C(sp)
+    stw  r0,  0x08(sp)
+    stw  r3,  0x0C(sp)
+    stw  r4,  0x10(sp)
+    stw  r5,  0x14(sp)
+    stw  r6,  0x18(sp)
+    stw  r7,  0x1C(sp)
+    stw  r8,  0x20(sp)
+    stw  r9,  0x24(sp)
+    stw  r10, 0x28(sp)
+    stw  r11, 0x2C(sp)
+    stw  r12, 0x30(sp)
+    mfcr r0
+    stw  r0,  0x34(sp)
+    mfctr r0
+    stw  r0,  0x38(sp)
+    mflr r0
+    stw  r0,  0x40(sp)
 
-    // call 
     bl printStageID;
 
-    // restore r3 and r4
-    stw r3, 0x4(r1);
-    stw r4, 0x8(r1);
-}*/
+    // restore volatile registers
+    lwz  r0,  0x38(sp)
+    mtctr r0
+    lwz  r0,  0x34(sp)
+    mtcr r0
+    lwz  r0,  0x40(sp)
+    mtlr r0
+    lwz  r0,  0x08(sp)
+    lwz  r3,  0x0C(sp)
+    lwz  r4,  0x10(sp)
+    lwz  r5,  0x14(sp)
+    lwz  r6,  0x18(sp)
+    lwz  r7,  0x1C(sp)
+    lwz  r8,  0x20(sp)
+    lwz  r9,  0x24(sp)
+    lwz  r10, 0x28(sp)
+    lwz  r11, 0x2C(sp)
+    lwz  r12, 0x30(sp)
+    addi sp,  sp, 0x3C
+}
