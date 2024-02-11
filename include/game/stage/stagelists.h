fd04a92e63d9638f164216a00cab6cefd1d01dd7
Swift <swiftshineexists@gmail.com>
2024-02-10 21:59:30 -0600

    stage list beginning

---
+++ include/game/stage/stagelists.h
@@ -0,0 +1,18 @@
+#ifndef FLUFF_STAGE_LIST_H
+#define FLUFF_STAGE_LIST_H
+
+enum StageIDs {
+    Stage_WorldMap          = 0,
+    // Grass Land
+    Stage_FountainGardens   = 101,
+    Stage_FlowerFields      = 102,
+    Stage_RainbowFalls      = 103,
+    Stage_BigBeanVine       = 104,
+    Stage_MoleHole          = 105,
+    Stage_WeirdWoods        = 106,
+
+    // Bossfights
+    Stage_Fangora           = 301,
+};
+
+#endif
