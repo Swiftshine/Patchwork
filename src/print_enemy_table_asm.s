5386358be21335fb9f7f8e3b671e5ca3733b670b
Swift <swiftshineexists@gmail.com>
2024-02-12 14:28:48 -0600

    proper enemytable printing

--- src/print_enemy_table_asm.s
+++ src/print_enemy_table_asm.s
@@ -2,85 +2,80 @@
 #include <kamek/kamek_asm.S>
 
 .data
-printName:  .string "table->name: %s\n\0"
+printSeparator: .string "**********\n\0"
+printID:    .string "table->enemyID: %d\n\0"
 printCtor:  .string "table->ctor/caller: %p\n\0"
 printLinkedEnemyName: .string "table->linkedEnemyName: %s\n\0"
 printPath:  .string "table->path: %s\n\0"
-#printID:    .string "table->someID: %d\n\0"
+printName:  .string "table->name: %s\n\0"
+enemyTable: .int 0
 .text
 
-# a comment
-// psuedocode
-
-# print name
-# hook into `getEnemyNameByID()`
+# hook into 'getEnemyNameByID()'
 kmBranchDef 0x8012E958
-    # original instruction
-    lwz r3, 0x4(r3)
-
-    # r3 is currently the ptr to the name string
+    # r3 is currently an EnemyTable** (pointer to a pointer)
     SaveVolatileRegisters
 
-    // r4 = r3
-    mr r4, r3
-    // r3 = &printName
+    lis r3, printSeparator@ha
+    addi r3, r3, printSeparator@l
+    // OSReport()
+    kamek_bl 0x806649C0
+
+    RefreshVolatileRegisters
+    lis r4, enemyTable@ha
+    addi r4, r4, enemyTable@l
+    stw r3, 0x0(r4)
+        # Print ID
+    lwz r4, 0x0(r4) # address
+    lwz r4, 0x0(r4) # actual value
+    lis r3, printID@ha
+    addi r3, r3, printID@l
+
+    
+    kamek_bl 0x806649C0
+
+    RefreshVolatileRegisters
+        # Print name
+    lis r4, enemyTable@ha
+    addi r4, r4, enemyTable@l
+    lwz r4, 0x0(r4)
+    lwz r4, 0x4(r4)
     lis r3, printName@ha
     addi r3, r3, printName@l
-
-    // OSReport(r3, r4)
     kamek_bl 0x806649C0
 
-    # done
-    RestoreVolatileRegisters
-    kamek_b 0x8012E95C # 'blr'
-
-# print constructor (or constructor caller) address
-# hook into `getEnemyConstructorByID()`
-kmBranchDef 0x8012D484
-    # original instruction
-    lwz r3, 0x8(r3)
-
-    # r3 is currently the ctorCaller addr (the one we want to print)
-    SaveVolatileRegisters
-
-    // r4 = r3
-    mr r4, r3
-    // r3 = &printCtor
+    RefreshVolatileRegisters
+        # Print constructor
+    lis r4, enemyTable@ha
+    addi r4, r4, enemyTable@l
+    lwz r4, 0x0(r4)
+    lwz r4, 0x8(r4)
     lis r3, printCtor@ha
     addi r3, r3, printCtor@l
-
-    // OSreport(r3, r4)
     kamek_bl 0x806649C0
 
-    # done
-    RestoreVolatileRegisters
-    kamek_b 0x8012D488 # 'blr'
-
-# print related enemy name
-# hook into `getEnemyLinkedEnemyNameByID()`
-kmBranchDef 0x8012E970
-    # original instruction
-    lwz r3, 0xC(r3)
-
-    # you get it by this point. just look at the EnemyTable struct
-    SaveVolatileRegisters
-    mr r4, r3
+    RefreshVolatileRegisters
+        # Print linked enemy name
+    lis r4, enemyTable@ha
+    addi r4, r4, enemyTable@l
+    lwz r4, 0x0(r4)
+    lwz r4, 0xC(r4)
     lis r3, printLinkedEnemyName@ha
     addi r3, r3, printLinkedEnemyName@l
     kamek_bl 0x806649C0
-    RestoreVolatileRegisters
-    kamek_b 0x8012e974 # 'blr'
 
-# print enemy resource path
-kmBranchDef 0x8012e988
-    # original instruction
-    lwz r3, 0x10(r3)
-    SaveVolatileRegisters
-    mr r4, r3
+    RefreshVolatileRegisters
+        # Print filepath
+    lis r4, enemyTable@ha
+    addi r4, r4, enemyTable@l
+    lwz r4, 0x0(r4)
+    lwz r4, 0x10(r4)
     lis r3, printPath@ha
     addi r3, r3, printPath@l
     kamek_bl 0x806649C0
-    RestoreVolatileRegisters
-    kamek_b 0x8012e98C
 
-# currently no in-game function to return an EnemyTable.someID but dw about it for now
+    # done, restore functionality of original function
+    RestoreVolatileRegisters
+    # original instruction
+    lwz r3, 0x4(r3)
+    kamek_b 0x8012E95C # jump to next instruction
