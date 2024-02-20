d38d3a45b04d868616c6cbf3599998eb0c7cdcb3
Swift <swiftshineexists@gmail.com>
2024-02-19 18:25:29 -0600

    GFA FileEntry hash calculation script

---
+++ tools/hash_calc.py
@@ -0,0 +1,20 @@
+import sys
+
+def calc_hash(filename: str):
+    calculated_hash = 0
+    for char in filename:
+        # '% (2**32)' to ensure that 32-bits
+        calculated_hash = (ord(char) + calculated_hash * 0x89) % (2**32)
+    return calculated_hash
+
+def main():
+    arg_count = len(sys.argv) - 1
+    if arg_count != 1:
+        print("Usage: " + sys.argv[0] + " <filename>")
+        sys.exit(1)
+
+    print(hex(calc_hash(sys.argv[1])))
+
+
+if __name__ == '__main__':
+    main()
