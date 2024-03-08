ebba6b992ae2637421ee57de2cb99da043e37763
Swift <85366980+Swiftshine@users.noreply.github.com>
2024-03-07 19:17:27 -0600

    Update rk5_loader.cpp

--- src/loader/rk5_loader.cpp
+++ src/loader/rk5_loader.cpp
@@ -18,18 +18,6 @@ void freeAdapter(void *buffer, bool isForCode, const loaderFunctions *funcs) {
     funcsEx->gflFree(buffer);
 }
 
-const loaderFunctions functions_ntsc = {
-    (OSReport_t) 0x806649C0,
-    (OSFatal_t) 0x80666CB0,
-    (DVDConvertPathToEntrynum_t) 0x8066FD30,
-    (DVDFastOpen_t) 0x80670040,
-    (DVDReadPrio_t) 0x80670410,
-    (DVDClose_t) 0x806701D0,
-    (sprintf_t) 0x80694500,
-    (KamekAlloc_t) 0x80644060,
-    (KamekFree_t) 0x806441C4
-};
-
 const loaderFunctionsEx functions_ex_ntsc = {
     {(OSReport_t) 0x806649C0,
     (OSFatal_t) 0x80666CB0,
