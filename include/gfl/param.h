c0efd335486129642a3d68ab5d96e85fb24abca8
Swift <swiftshineexists@gmail.com>
2024-03-19 10:26:56 -0500

    further gfl param documentation

--- include/gfl/param.h
+++ include/gfl/param.h
@@ -5,33 +5,121 @@
 #pragma pack(push, 1)
 #include <types.h>
 
+
+// For the sake of consistency and clarity,
+// DO NOT use the terms 'name' and 'label' interchangeably.
+// They are not the same thing.
+
+
+// A label cannot exceed 32 bytes.
+// This includes the null terminator.
+
 namespace GFL {
+namespace Param {
+    /* begin custom stuctures */
 
-    // offset, size, comment
+    // An unofficial base structure for GFL parameters.
+    class ParamBase { };
 
-    typedef union {
-        u32 uinteger;   // size - 0x4
-        s32 sinteger;   // size - 0x4
-        f32 floating;   // size - 0x4
-        char* str;      // size - 0x4
-        bool boolean;   // size - 0x1
-    } ParamValue;
+    /* end custom structures */
 
-    struct Param {
-        void* vtable;       // 0x0, 0x4
-        void* baseCallback; // 0x4, 0x4
-        u8 unk[0x20];       // 0x8, 0x20
-        ParamValue value;   // 0x28, 0x4
-        void* paramType2;   // 0x2C, 0x4, I still don't know what this is
+
+    // Confirms if the given label and hash correspond to those of
+    // the GFL param.
+    static bool isValid(ParamBase* param, char* label, u32 hash);
+    // Assigns a label and hash to a GFL param.
+    static void set(ParamBase* param, char* label, u32 hash);
+
+
+    class ParamPtr {
+        // this one in particular is relatively vague.
     };
 
-    struct ParamGroup {
-        GFL::Param base;    // 0x0, 0x30
-        char* _30;          // 0x30, 0x4
+    class ParamBool : public ParamBase {
+    public:
+        ParamBool(char* label);
+        ~ParamBool();
+    public:
+        void* vtable;           // 0x0, 0x4
+        char label[0x20];       // 0x4, 0x20
+        u32 hash;               // 0x24, 0x4
+        bool value;             // 0x28, 0x1
+        u8 padding[3];          // 0x29, 0x3
+        GFL::BasicString* _2C;  // 0x2C, 0x4
     };
 
-    
-}
+
+    class ParamS32 {
+    public:
+        ParamS32(char* label);
+        ~ParamS32();
+        
+        void print(u32 unk1, u32 unk2);
+    public:
+        void* vtable;           // 0x0, 0x4
+        char label[0x20];       // 0x4, 0x20
+        u32 hash;               // 0x24, 0x4
+        s32 value;              // 0x28, 0x4
+        GFL::BasicString* _2C;  // 0x2C, 0x4
+    };
+
+    class ParamF32 {
+    public:
+        ParamF32(char* label);
+
+        void print(u32 unk1, u32 unk2);
+    public:
+        void* vtable;           // 0x0, 0x4
+        char label[0x20];       // 0x4, 0x20
+        u32 hash;               // 0x24, 0x4
+        f32 value;              // 0x28, 0x4
+        GFL::BasicString* _2C;  // 0x2C, 0x4
+    };
+
+    class ParamStr {
+    public:
+        ParamStr(char* label);
+        ~ParamStr();
+
+        void print(u32 unk1, u32 unk2);
+    public:
+        void* vtable;               // 0x0, 0x4
+        char label[0x20];           // 0x4, 0x20
+        u32 hash;                   // 0x24, 0x4
+        GFL::BasicString* value;    // 0x28, 0x4
+        GFL::BasicString* _2C;      // 0x2C, 0x4
+        GFL::BasicString* _30;      // 0x30, 0x4
+    };
+
+
+    class ParamGroup {
+    public:
+        // ParamGroup();
+        ~ParamGroup();
+
+        // Prints contents.
+        void print(u32 unk1, u32 unk2);
+
+        // Returns a ParamGroup* by its label.
+        static ParamGroup* getByLabel(char* label);
+        // Generates a ParamGroup* with a label.
+        static ParamGroup* fromLabel(char* label);
+
+        // Returns an S32 param by label.
+        ParamS32* paramS32ByLabel(char* label);
+
+    public:
+        void* vtable;               // 0x0, 0x4
+        char label[0x20];           // 0x4, 0x20
+        u32 hash;                   // 0x24, 0x4
+        void* value;                // 0x28, 0x4
+        GFL::BasicString* _2C;      // 0x2C, 0x4
+        void* _30;                  // 0x30, 0x4
+    };
+
+
+} // namespace Param
+} // namespace GFL
 
 #endif
 #pragma pack(pop)
