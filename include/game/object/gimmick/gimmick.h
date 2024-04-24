4e7dd4f24e102a5997891678e3eb7be13ccbbdbd
Swift <swiftshineexists@gmail.com>
2024-04-24 15:45:12 -0500

    gimmick struct

--- include/game/object/gimmick/gimmick.h
+++ include/game/object/gimmick/gimmick.h
@@ -7,23 +7,45 @@
 #include "gfl/gfl.h"
 
 // where gimmick tables are observed to start
-#define GIMMICK_TABLES_ADDRS 0x8083B474
+#define GIMMICK_TABLES_ADDRS 0x8083b468
+// #define GIMMICK_TABLES_ADDRS 0x8083B474
 
-// size: 0x80
+
+// size: 0x130 (i think)
 class Gimmick : FlfGameObj {
 public:
-
+    Gimmick();
+    ~Gimmick();
 public:
-
+    u32 _80;
+    u32 _84;
+    u32 _88;
+    Vec3f _8C;
+    Vec3f _98;
+    u8 _A4;
+    u8 pad1[3];
+    u32 _A8;
+    u8 _AC;
+    u8 pad2[3];
+    u32 _B0;
+    u32 _B4;
+    u32 _B8;
+    void* _BC[5];
+    void* _D0[5];
+    gfl::String::BasicString strings[5];
+    u32 _120;
+    u32 _124;
+    u32 _128;
+    u32 _12C;
 };
 
 // size unknown
 class GimmickTable {
 public:
     char* description;  // ShiftJIS, not ASCII
-    char* objectName;
+    char* name;
     void (*constructor)();
-    u32 someID;         // unsure if this is really an ID
+    void (*stringRelatedFunction)();
 };
 
 #endif
